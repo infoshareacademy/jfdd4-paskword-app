0938347b17eff0405eccaf41434bf77ea7a391f6
Agata Kulbicka <agata_kulbicka@wp.pl>
2016-10-12 23:18:22 +0200

    rearranged directory structure

---
+++ src/data/offices.js
@@ -0,0 +1,23 @@
+vererinaryOffices = [
+    {
+        "officeName": "Mruczuś",
+        "officeAddress": "ul.Bonifacego 14, 90-098 Gdańsk",
+        "vetsNumber": 4
+    },
+
+    {
+        "officeName": "Klakier",
+        "officeAddress": "ul.Gargamela 2, 90-098 Gdańsk",
+        "vetsNumber": 6
+    },
+    {
+        "officeName": "Reksio",
+        "officeAddress": "ul. Bolka i Lolska 3, 90-098 Gdańsk",
+        "vetsNumber": 9
+    },
+    {
+        "officeName": "Filonek",
+        "officeAddress": "ul.Ogonowa, 90-098 Gdańsk",
+        "vetsNumber": 2
+    }
+]
