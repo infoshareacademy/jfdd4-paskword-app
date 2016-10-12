321b70a52d9b7fb3a5fd0f3ac8cd21f837997e03
Agata Kulbicka <agata_kulbicka@wp.pl>
2016-10-12 16:52:59 +0200

    added props children to one veterinary office pattern

--- src/offices/Offices.js
+++ src/offices/Offices.js
@@ -3,13 +3,14 @@ import React from 'react';
 
 export default class Offices extends React.Component {
 
+    
     render(){
         return(
             <div>
-                <img src='#' alt="logo"></img>
-                <span>Nazwa gabinetu:</span><span>Mruczuś</span>
-                <span>Adres:</span><span>ul. Kocia14, 90-098 Gdańsk</span>
-                <span>Liczba lekarzy:</span><span>4</span>
+                <img src='#' alt="logo">{this.props.children}</img>
+                <span>Nazwa gabinetu:</span><span>{this.props.children}</span>
+                <span>Adres:</span><span>{this.props.children}</span>
+                <span>Liczba lekarzy:</span><span>{this.props.children}</span>
             </div>
         )
     }
