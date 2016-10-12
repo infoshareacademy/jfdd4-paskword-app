31cee29e14dd28935f71636fef72d683bfed2c5b
Katarzyna Kaldunska <k.kaduska@gmail.com>
2016-10-12 15:08:53 +0200

    add Links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router';
+import Vet from 'vet/Vet';
+import Advice from 'advice/Advice';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <p>
+              <Link to={`/vet`}>Nasi weterynarze</Link>
+              <Link to={`/advice`}>Porady</Link>
+          </p>
+
+      {this.props.children}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { Router, Route, browserHistory} from 'react-router';
+
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="/vet" component={Vet} />
+      <Route path="/advice" component={Advice} />
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
