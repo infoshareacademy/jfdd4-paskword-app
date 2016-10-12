9b7ce188cc51cd09afd5e54e40d8efcdf469b4fd
Agata Kulbicka <agata_kulbicka@wp.pl>
2016-10-12 16:46:17 +0200

    created pattern for one veterinary office

--- src/offices/Offices.js
+++ src/offices/Offices.js
@@ -1,2 +1,18 @@
 import React from 'react';
 // import Map from './map/Map';
+
+export default class Offices extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <img src='#' alt="logo"></img>
+                <span>Nazwa gabinetu:</span><span>Mruczuś</span>
+                <span>Adres:</span><span>ul. Kocia14, 90-098 Gdańsk</span>
+                <span>Liczba lekarzy:</span><span>4</span>
+            </div>
+        )
+    }
+
+}
+
