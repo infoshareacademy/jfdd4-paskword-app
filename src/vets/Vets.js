de1458cbcd172c39ad6e5a29e96c32016b70d826
Katarzyna Kaldunska <k.kaduska@gmail.com>
2016-10-13 17:20:37 +0200

    FDPF-73 add advices to vets

--- src/vets/Vets.js
+++ src/vets/Vets.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import vetsData from '../data/vets';
+import advices from '../data/advices'
 import './Vets.css';
 import {Media} from 'react-bootstrap';
 
@@ -9,38 +10,61 @@ export default class Vets extends React.Component {
         super();
         this.state = {
             vets: [],
+            advices: [],
             isLoading: true
-        }
+        };
+        this._assignAdvices = this._assignAdvices.bind(this);
     }
 
     componentWillMount() {
         this.setState({
             vets: vetsData,
+            advices: advices,
             isLoading: false
         });
+        this._assignAdvices();
+    }
+
+    _assignAdvices() {
+        var vets = this.state.vets;
+        var vetNumber = this.state.vets.length;
+
+        var vetsWith = vets.map(function(vet, i) {
+           vet[i].advices = [];
+
+        });
+        console.log(vetsWith);
+        vets.advices.forEach(function(advice) {
+            var randomNumber = Math.floor((Math.random() * vetNumber) +1);
+            vets[randomNumber].advices.push(advice);
+            }
+        )
     }
 
     render() {
-        var allVetsData = this.state.vets;
+        var allVetsData = this.state.vets,
+            advices = this.state.advices;
 
         return (
             <div>
+                {console.log(advices)};
                 {this.state.isLoading ? 'Loading list of our vets...' : null}
                 {allVetsData.map(function(vet) {
                     return (
                         <Media>
                             <Media.Left align="top">
-                                <img src={vet.photo.src}/>
+                                <img src={vet.photo} alt={vet.firstName}/>
                             </Media.Left>
                             <Media.Body>
                                 <Media.Heading> {vet.firstName} {vet.lastName} </Media.Heading>
                                 <p>Przychodnia: {vet.office}</p>
-                                <p>Porada lekarza: </p>
+                                <p>Liczba porad lekarza: {vet.advices.length}</p>
                             </Media.Body>
                         </Media>
                     )
                 })}
             </div>
+
         )
     }
 }
