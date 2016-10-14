6e03953a10720a7b9bfb27ac2e2b7ec2012f940a
Agata Kulbicka <agata_kulbicka@wp.pl>
2016-10-14 15:31:45 +0200

    added GPS points to existing vets office data

--- src/data/offices.js
+++ src/data/offices.js
@@ -1,28 +1,44 @@
-var  data = [
+var data = [
     {
         "id": 1,
         "officeName": "Mruczuś",
         "officeAddress": "ul.Bonifacego 14, 90-098 Gdańsk",
-        "vetsNumber": 4
+        "vetsNumber": 4,
+        "coordinates": {
+            latitude: 54.32853723970533,
+            longitude: 18.527755737304688
+        }
     },
 
     {
         "id": 2,
         "officeName": "Klakier",
         "officeAddress": "ul.Gargamela 2, 90-098 Gdańsk",
-        "vetsNumber": 6
+        "vetsNumber": 6,
+        "coordinates": {
+            latitude: 54.26001043274683,
+            longitude: 18.702850341796875
+        }
     },
     {
         "id": 3,
         "officeName": "Reksio",
         "officeAddress": "ul. Bolka i Lolska 3, 90-098 Gdańsk",
-        "vetsNumber": 9
+        "vetsNumber": 9,
+        "coordinates": {
+            latitude: 54.350453745002405,
+            longitude: 18.664827346801758
+        }
     },
     {
         "id": 4,
         "officeName": "Filonek",
         "officeAddress": "ul.Ogonowa, 90-098 Gdańsk",
-        "vetsNumber": 2
+        "vetsNumber": 2,
+        "coordinates": {
+            latitude: 54.3438500007626,
+            longitude: 18.676843643188477
+        }
     }
 ];
 
