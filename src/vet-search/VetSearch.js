5aa59e90390ac6121817064441b4aa5d26d8c9a8
Agata Kulbicka <agata_kulbicka@wp.pl>
2016-10-18 17:08:31 +0200

    counting distance in progress

--- src/vet-search/VetSearch.js
+++ src/vet-search/VetSearch.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import GoogleMap from 'google-map-react';
+import Place from '../map/place/Place'
 // import Place from './place/Place';
 import styles from './vet-search-style.css'
 
@@ -12,19 +13,35 @@ import {
     FormControl,
     HelpBlock
 } from 'react-bootstrap';
+import Data from '../data/offices'
 
 
 export default class VetSearch extends React.Component {
     constructor() {
         super();
-        this.state =  {
-            nearestVet: []
+        this.state = {
+            officesData: [],
+            currentCoordinates: []
         }
-        
-        this._onClick  = this._onClick.bind(this);
+
+        this._onClick = this._onClick.bind(this);
     }
 
-    _onClick = ({x, y, lat, lng, event}) => console.log(x, y, lat, lng, event)
+    componentWillMount() {
+        this.setState = {
+            officesData: Data,
+            currentCoordinates: {}
+        }
+    }
+
+    _onClick = (markerPosition, mousePosition, markerProps) => {
+        console.log('mouse',mousePosition,'marker', markerPosition);
+        const x = markerPosition.x;
+        const y = markerPosition.y;
+        const distanceKoef = markerProps.text !== 'A' ? 1.5 : 1;
+        return distanceKoef * Math.sqrt((x - mousePosition.x) * (x - mousePosition.x) + (y - mousePosition.y) * (y - mousePosition.y));
+
+    }
 
     render() {
         return (
@@ -36,11 +53,17 @@ export default class VetSearch extends React.Component {
                                 key: 'AIzaSyCJSyocAtUnWSKhjyqZlJtmaf_afdJcOkA',
                                 language: 'pl'
                             }}
-                               onClick={this._onClick}
-                               center={[54.35118909616142, 18.644957542419434]}
-                               zoom={9}>
+                            onClick={this._onClick}
+                            center={[54.35118909616142, 18.644957542419434]}
+                            zoom={9}>
 
-                    </GoogleMap>
+                            <Place
+                                lat={54.35118909616142} lng={18.644957542419434} text={'A'} zIndex={2}
+                            >
+                                
+                            </Place>
+                            <Place></Place>
+                        </GoogleMap>
                     </div>
                 </Row>
             </Grid>
