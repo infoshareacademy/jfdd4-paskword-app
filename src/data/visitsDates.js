f85a3a6c6df56dc5bbe448c394bfa8f8f007c932
Sandra Kowalska <sandrinkha@gmail.com>
2016-10-24 12:00:08 +0200

    added new dates to visitDates

--- src/data/visitsDates.js
+++ src/data/visitsDates.js
@@ -44,14 +44,14 @@ var data =[
     },
     {
         "title": 'zajęty termin',
-        "visitId": 2,
+        "visitId": 1,
         "vetId": 2,
         "start":new Date(2016, 9, 24,7,0,0),
         "end":new Date(2016, 9, 24,8,0,0)
     },
     {
         "title": 'zajęty termin',
-        "visitId": 9,
+        "visitId": 2,
         "vetId": 2,
         "start":new Date(2016, 9, 24,10,0,0),
         "end":new Date(2016, 9, 24,11,0,0)
@@ -59,44 +59,555 @@ var data =[
     {
         "title": 'zajęty termin',
         "visitId": 3,
+        "vetId": 2,
+        "start":new Date(2016, 9, 24,12,0,0),
+        "end":new Date(2016, 9, 24,13,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 4,
+        "vetId": 2,
+        "start":new Date(2016, 9, 25,10,0,0),
+        "end":new Date(2016, 9, 25,12,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 5,
+        "vetId": 2,
+        "start":new Date(2016, 9, 25,13,0,0),
+        "end":new Date(2016, 9, 25,14,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 6,
+        "vetId": 2,
+        "start":new Date(2016, 9, 27,7,0,0),
+        "end":new Date(2016, 9, 27,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 7,
+        "vetId": 2,
+        "start":new Date(2016, 9, 27,9,0,0),
+        "end":new Date(2016, 9, 27,10,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 8,
+        "vetId": 2,
+        "start":new Date(2016, 9, 27,10,0,0),
+        "end":new Date(2016, 9, 27,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 9,
+        "vetId": 2,
+        "start":new Date(2016, 9, 31,7,0,0),
+        "end":new Date(2016, 9, 31,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 1,
         "vetId": 3,
-        "start":new Date(2016, 9, 25,9,0,0),
-        "end":new Date(2016, 9, 25,10,0,0)
+        "start":new Date(2016, 9, 24,7,0,0),
+        "end":new Date(2016, 9, 24,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 2,
+        "vetId": 3,
+        "start":new Date(2016, 9, 24,10,0,0),
+        "end":new Date(2016, 9, 24,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 3,
+        "vetId": 3,
+        "start":new Date(2016, 9, 24,12,0,0),
+        "end":new Date(2016, 9, 24,13,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 4,
+        "vetId": 3,
+        "start":new Date(2016, 9, 25,10,0,0),
+        "end":new Date(2016, 9, 25,12,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 5,
+        "vetId": 3,
+        "start":new Date(2016, 9, 25,13,0,0),
+        "end":new Date(2016, 9, 25,14,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 6,
+        "vetId": 3,
+        "start":new Date(2016, 9, 27,7,0,0),
+        "end":new Date(2016, 9, 27,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 7,
+        "vetId": 3,
+        "start":new Date(2016, 9, 27,9,0,0),
+        "end":new Date(2016, 9, 27,10,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 8,
+        "vetId": 3,
+        "start":new Date(2016, 9, 27,10,0,0),
+        "end":new Date(2016, 9, 27,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 9,
+        "vetId": 3,
+        "start":new Date(2016, 9, 31,7,0,0),
+        "end":new Date(2016, 9, 31,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 1,
+        "vetId": 4,
+        "start":new Date(2016, 9, 24,7,0,0),
+        "end":new Date(2016, 9, 24,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 2,
+        "vetId": 4,
+        "start":new Date(2016, 9, 24,10,0,0),
+        "end":new Date(2016, 9, 24,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 3,
+        "vetId": 4,
+        "start":new Date(2016, 9, 24,12,0,0),
+        "end":new Date(2016, 9, 24,13,0,0)
     },
     {
         "title": 'zajęty termin',
         "visitId": 4,
         "vetId": 4,
-        "start":new Date(2016, 9, 24,9,0,0),
+        "start":new Date(2016, 9, 25,10,0,0),
+        "end":new Date(2016, 9, 25,12,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 5,
+        "vetId": 4,
+        "start":new Date(2016, 9, 25,15,0,0),
+        "end":new Date(2016, 9, 25,17,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 6,
+        "vetId": 4,
+        "start":new Date(2016, 9, 27,7,0,0),
+        "end":new Date(2016, 9, 27,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 7,
+        "vetId": 4,
+        "start":new Date(2016, 9, 27,9,0,0),
+        "end":new Date(2016, 9, 27,10,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 8,
+        "vetId": 4,
+        "start":new Date(2016, 9, 27,10,0,0),
+        "end":new Date(2016, 9, 27,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 9,
+        "vetId": 4,
+        "start":new Date(2016, 9, 31,7,0,0),
+        "end":new Date(2016, 9, 31,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 1,
+        "vetId": 5,
+        "start":new Date(2016, 9, 24,7,0,0),
+        "end":new Date(2016, 9, 24,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 2,
+        "vetId": 5,
+        "start":new Date(2016, 9, 24,10,0,0),
         "end":new Date(2016, 9, 24,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 3,
+        "vetId": 5,
+        "start":new Date(2016, 9, 24,12,0,0),
+        "end":new Date(2016, 9, 24,13,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 4,
+        "vetId": 5,
+        "start":new Date(2016, 9, 25,10,0,0),
+        "end":new Date(2016, 9, 25,12,0,0)
     },
     {
         "title": 'zajęty termin',
         "visitId": 5,
         "vetId": 5,
-        "start":new Date(2016, 9, 26,8,0,0),
-        "end":new Date(2016, 9, 26,9,0,0)
+        "start":new Date(2016, 9, 25,14,0,0),
+        "end":new Date(2016, 9, 25,16,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 6,
+        "vetId": 5,
+        "start":new Date(2016, 9, 27,7,0,0),
+        "end":new Date(2016, 9, 27,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 7,
+        "vetId": 5,
+        "start":new Date(2016, 9, 27,9,0,0),
+        "end":new Date(2016, 9, 27,10,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 8,
+        "vetId": 5,
+        "start":new Date(2016, 9, 27,10,0,0),
+        "end":new Date(2016, 9, 27,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 9,
+        "vetId": 5,
+        "start":new Date(2016, 9, 31,7,0,0),
+        "end":new Date(2016, 9, 31,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 1,
+        "vetId": 6,
+        "start":new Date(2016, 9, 24,7,0,0),
+        "end":new Date(2016, 9, 24,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 2,
+        "vetId": 6,
+        "start":new Date(2016, 9, 24,10,0,0),
+        "end":new Date(2016, 9, 24,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 3,
+        "vetId": 6,
+        "start":new Date(2016, 9, 24,12,0,0),
+        "end":new Date(2016, 9, 24,13,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 4,
+        "vetId": 6,
+        "start":new Date(2016, 9, 25,15,0,0),
+        "end":new Date(2016, 9, 25,16,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 5,
+        "vetId": 6,
+        "start":new Date(2016, 9, 25,11,0,0),
+        "end":new Date(2016, 9, 25,12,0,0)
     },
     {
         "title": 'zajęty termin',
         "visitId": 6,
         "vetId": 6,
-        "start":new Date(2016, 9, 26,8,0,0),
-        "end":new Date(2016, 9, 26,9,0,0)
+        "start":new Date(2016, 9, 27,7,0,0),
+        "end":new Date(2016, 9, 27,8,0,0)
     },
     {
         "title": 'zajęty termin',
         "visitId": 7,
+        "vetId": 6,
+        "start":new Date(2016, 9, 27,9,0,0),
+        "end":new Date(2016, 9, 27,10,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 8,
+        "vetId": 6,
+        "start":new Date(2016, 9, 27,10,0,0),
+        "end":new Date(2016, 9, 27,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 9,
+        "vetId": 6,
+        "start":new Date(2016, 9, 31,7,0,0),
+        "end":new Date(2016, 9, 31,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 1,
+        "vetId": 7,
+        "start":new Date(2016, 9, 24,7,0,0),
+        "end":new Date(2016, 9, 24,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 2,
         "vetId": 7,
         "start":new Date(2016, 9, 24,10,0,0),
         "end":new Date(2016, 9, 24,11,0,0)
     },
     {
         "title": 'zajęty termin',
+        "visitId": 3,
+        "vetId": 7,
+        "start":new Date(2016, 9, 24,12,0,0),
+        "end":new Date(2016, 9, 24,13,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 4,
+        "vetId": 7,
+        "start":new Date(2016, 9, 25,10,0,0),
+        "end":new Date(2016, 9, 25,12,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 5,
+        "vetId": 7,
+        "start":new Date(2016, 9, 25,16,0,0),
+        "end":new Date(2016, 9, 25,17,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 6,
+        "vetId": 7,
+        "start":new Date(2016, 9, 27,7,0,0),
+        "end":new Date(2016, 9, 27,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 7,
+        "vetId": 7,
+        "start":new Date(2016, 9, 27,9,0,0),
+        "end":new Date(2016, 9, 27,10,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 8,
+        "vetId": 7,
+        "start":new Date(2016, 9, 27,10,0,0),
+        "end":new Date(2016, 9, 27,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 9,
+        "vetId": 7,
+        "start":new Date(2016, 9, 31,7,0,0),
+        "end":new Date(2016, 9, 31,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 1,
+        "vetId": 8,
+        "start":new Date(2016, 9, 24,7,0,0),
+        "end":new Date(2016, 9, 24,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 2,
+        "vetId": 8,
+        "start":new Date(2016, 9, 24,10,0,0),
+        "end":new Date(2016, 9, 24,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 3,
+        "vetId": 8,
+        "start":new Date(2016, 9, 24,12,0,0),
+        "end":new Date(2016, 9, 24,13,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 4,
+        "vetId": 8,
+        "start":new Date(2016, 9, 25,10,0,0),
+        "end":new Date(2016, 9, 25,12,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 5,
+        "vetId": 8,
+        "start":new Date(2016, 9, 25,17,0,0),
+        "end":new Date(2016, 9, 25,18,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 6,
+        "vetId": 8,
+        "start":new Date(2016, 9, 27,7,0,0),
+        "end":new Date(2016, 9, 27,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 7,
+        "vetId": 8,
+        "start":new Date(2016, 9, 27,9,0,0),
+        "end":new Date(2016, 9, 27,10,0,0)
+    },
+    {
+        "title": 'zajęty termin',
         "visitId": 8,
         "vetId": 8,
-        "start":new Date(2016, 9, 25,7,0,0),
-        "end":new Date(2016, 9, 25,8,0,0)
+        "start":new Date(2016, 9, 27,10,0,0),
+        "end":new Date(2016, 9, 27,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 9,
+        "vetId": 8,
+        "start":new Date(2016, 9, 31,7,0,0),
+        "end":new Date(2016, 9, 31,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 1,
+        "vetId": 9,
+        "start":new Date(2016, 9, 24,7,0,0),
+        "end":new Date(2016, 9, 24,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 2,
+        "vetId": 9,
+        "start":new Date(2016, 9, 24,10,0,0),
+        "end":new Date(2016, 9, 24,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 3,
+        "vetId": 9,
+        "start":new Date(2016, 9, 24,12,0,0),
+        "end":new Date(2016, 9, 24,13,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 4,
+        "vetId": 9,
+        "start":new Date(2016, 9, 25,10,0,0),
+        "end":new Date(2016, 9, 25,12,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 5,
+        "vetId": 9,
+        "start":new Date(2016, 9, 25,15,0,0),
+        "end":new Date(2016, 9, 25,16,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 6,
+        "vetId": 9,
+        "start":new Date(2016, 9, 27,7,0,0),
+        "end":new Date(2016, 9, 27,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 7,
+        "vetId": 9,
+        "start":new Date(2016, 9, 27,9,0,0),
+        "end":new Date(2016, 9, 27,10,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 8,
+        "vetId": 9,
+        "start":new Date(2016, 9, 27,10,0,0),
+        "end":new Date(2016, 9, 27,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 9,
+        "vetId": 9,
+        "start":new Date(2016, 9, 31,7,0,0),
+        "end":new Date(2016, 9, 31,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 1,
+        "vetId": 10,
+        "start":new Date(2016, 9, 24,7,0,0),
+        "end":new Date(2016, 9, 24,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 2,
+        "vetId": 10,
+        "start":new Date(2016, 9, 24,10,0,0),
+        "end":new Date(2016, 9, 24,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 3,
+        "vetId": 10,
+        "start":new Date(2016, 9, 24,12,0,0),
+        "end":new Date(2016, 9, 24,13,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 4,
+        "vetId": 10,
+        "start":new Date(2016, 9, 25,10,0,0),
+        "end":new Date(2016, 9, 25,12,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 5,
+        "vetId": 10,
+        "start":new Date(2016, 9, 25,15,0,0),
+        "end":new Date(2016, 9, 25,16,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 6,
+        "vetId": 10,
+        "start":new Date(2016, 9, 27,7,0,0),
+        "end":new Date(2016, 9, 27,8,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 7,
+        "vetId": 10,
+        "start":new Date(2016, 9, 27,9,0,0),
+        "end":new Date(2016, 9, 27,10,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 8,
+        "vetId": 10,
+        "start":new Date(2016, 9, 27,10,0,0),
+        "end":new Date(2016, 9, 27,11,0,0)
+    },
+    {
+        "title": 'zajęty termin',
+        "visitId": 9,
+        "vetId": 10,
+        "start":new Date(2016, 9, 31,7,0,0),
+        "end":new Date(2016, 9, 31,8,0,0)
     },
 
 ];
